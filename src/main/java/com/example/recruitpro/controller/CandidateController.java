18fa47de55f4f922e855dfcc4670b4973c3d8978
Hiển Vinh <hthvinh48@gmail.com>
2026-09-21 01:13:43 +0700

    update on asm02 ss07

--- src/main/java/com/example/recruitpro/controller/CandidateController.java
+++ src/main/java/com/example/recruitpro/controller/CandidateController.java
@@ -1,9 +1,11 @@
 package com.example.recruitpro.controller;
 
 import com.example.recruitpro.dto.request.CandidateCreateDTO;
+import com.example.recruitpro.dto.response.ApiResponse;
 import com.example.recruitpro.entity.Candidate;
 import com.example.recruitpro.service.CandidateService;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,13 @@ public class CandidateController {
     }
 
     @PostMapping
-    public Candidate addCandidate(
+    public ResponseEntity<ApiResponse<Candidate>> addCandidate(
             @Valid @RequestBody CandidateCreateDTO candidateCreateDTO
     ) {
-        return candidateService.createCandidate(candidateCreateDTO);
+        return ResponseEntity.ok(new ApiResponse<>(
+                "SUCCESS",
+                "Tạo mới dữ liệu thành công",
+                candidateService.createCandidate(candidateCreateDTO)
+        ));
     }
 }
---
+++ src/main/java/com/example/recruitpro/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.recruitpro.exception;
+
+import com.example.recruitpro.dto.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach((error) -> {
+            String fieldName = error.getField();
+            String errorMsg = error.getDefaultMessage();
+
+            errors.put(fieldName, errorMsg);
+        });
+
+        return new ApiResponse<>("FAIL", "Dữ liệu đầu vào không hợp lệ", errors);
+    }
+}
